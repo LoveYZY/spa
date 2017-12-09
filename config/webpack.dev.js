fd8de6c418f0ae6513d53d57c5b8ee46a677ac3f
杨再勇 <1689655167@qq.com>
2017-12-09 10:40:43 +0800

    Init

---
+++ config/webpack.dev.js
@@ -0,0 +1,15 @@
+process.env.NODE_ENV = 'develop';
+const webpackMerge = require('webpack-merge');
+const common = require('./webpack.common');
+const root = require('./webpack.util');
+module.exports = webpackMerge(common, {
+    devtool: 'source-map',
+    output: {
+        path: root('dist'),
+        filename: 'js/[name].js'
+    },
+    devServer: {
+        historyApiFallback: true,
+        stats: 'minimal'
+    }
+});
